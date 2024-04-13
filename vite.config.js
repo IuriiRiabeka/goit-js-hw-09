2e7f1d97612515d3fa304cc5b4e4d91205e12edf
IuriiRiabeka <sam2fly7@gmail.com>
2024-04-13 21:23:51 +0300

    vite ipdated with uninstall

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-/*import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
@@ -21,37 +21,6 @@ export default defineConfig(({ command }) => {
             }
           },
           entryFileNames: 'commonHelpers.js',
-        },
-      },
-      outDir: '../dist',
-    },*/
-//plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-//};
-//});
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      global: {}, // Використовуємо одну змінну для глобальних змінних
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html').reduce((acc, path) => {
-          acc[path.replace(/^\.\/src\//, '')] = path;
-          return acc;
-        }, {}),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
         },
       },
       outDir: '../dist',
